559f546bd144fea2f026bedc9cf5ffbf08d4a39c
John Andrew Self <andrew.self31@gmail.com>
2023-08-24 16:02:32 -0400

    Testing..testing..check..1..2

--- ACME_System/src/ver1/HelloWorld.java
+++ ACME_System/src/ver1/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Eclipse");
-
-	}
+		System.out.println("John is working locally on his branch");	}
 
 }
